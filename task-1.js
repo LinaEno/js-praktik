5019317af734f4f6a576e6b92cf31211b29581bc
Yuliia Hlushko <y.glushko@outlook.com>
2022-12-05 15:00:29 +0200

    add task-2

--- task-1.js
+++ task-1.js
@@ -2,18 +2,27 @@
 // Перевірте, що першим символом цього рядка є буква 'a'.
 // Якщо це так - виведіть 'так', інакше виведіть 'ні'.
 
-const str = 'abcde';
+// const str = 'abcde';
 
-// if (str.startsWith('b')) {
+// // if (str.startsWith('b')) {
+// //     console.log('так');
+// // }
+// // else { console.log('ні') };
+
+// if (str[0] === 'a') {
 //     console.log('так');
 // }
-// else { console.log('ні') };
-
-if (str[0] === 'a') {
-    console.log('так');
-}
-else {
-    console.log('ні')
-};
+// else {
+//     console.log('ні')
+// };
 
 
+const str = 'Де помити мопед';
+
+const normalizeStr = str.toLowerCase().replaceAll(' ','');
+
+const reverseStr = normalizeStr.split('').reverse().join('');
+
+
+
+console.log(normalizeStr === reverseStr);
